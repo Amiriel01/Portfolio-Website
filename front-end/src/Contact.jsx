f23d4ed2c5c7ddec81ec77463bfb58aa3464f7e0
Sarah Quinney <quinney.sarah@gmail.com>
2024-02-18 10:26:20 -0600

    updated netlify form tag

--- front-end/src/Contact.jsx
+++ front-end/src/Contact.jsx
@@ -66,7 +66,7 @@ export default function Contact() {
                         </Col>
                     </Row>
                     {/* <Form onSubmit={handleSubmit}> */}
-                    <Form data-netlify="true" method="POST">
+                    <Form name='profile-website-contact-form' data-netlify="true" method="POST">
                         <Row className="mb-3" id='contact-info-container'>
                             <Form.Group as={Col}>
                                 <FloatingLabel
